5737297eb9e4c86fb4eaa805ccb436f976c503df
Hanjin Lee <jinifor@gmail.com>
2017-10-17 16:32:30 +0900

    common header

--- ngiiedu-client/src/components/common/layout/Header.js
+++ ngiiedu-client/src/components/common/layout/Header.js
@@ -3,6 +3,26 @@ import React from 'react';
 import { withRouter } from "react-router-dom";
 import { Link } from 'react-router-dom';
 
+import {List, ListItem} from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
+import Divider from 'material-ui/Divider';
+
+import IconButton from 'material-ui/IconButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import Badge from 'material-ui/Badge';
+import FontIcon from 'material-ui/FontIcon';
+
+import IconNotifications from 'material-ui/svg-icons/social/notifications';
+import IconChevronLeft from 'material-ui/svg-icons/navigation/chevron-left';
+import IconArrowBack from 'material-ui/svg-icons/navigation/arrow-back';
+import IconMoreVert from 'material-ui/svg-icons/navigation/more-vert';
+import IconHelpOutline from 'material-ui/svg-icons/action/help-outline';
+
+import Avatar from 'material-ui/Avatar';
+
+
 import './Header.css';
 
 class Header extends React.Component {
@@ -14,14 +34,57 @@ class Header extends React.Component {
   render() {
     return (
       <header id="header">
-        <div className={this.props.wide ? "inner wide" : "inner"}>
-          <div className="tb_left">
-            <Link to="/">NGII-EDU LOGO</Link>
+        <div className={this.props.wide ? "inner wide" : "inner"} style={{display: 'flex'}}>
+
+          <div style={{flex: 1, marginLeft: 0, paddingTop: 20, paddingBottom: 20}}>
+            <Link to="/course"><h3>NGII-EDU LOGO</h3></Link>
           </div>
-          <div className="tb_right">
+
+          <div style={{flex: 4}}>
           </div>
+
+
+          <div style={{flex: 1, marginRight: 0}}>
+            <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+              <Badge
+                badgeContent={10}
+                secondary={true}
+                badgeStyle={{top: 15, right: 12}}
+                style={{padding: '12px 12px 0 0', margin: 'auto 10px'}}
+              >
+                <IconButton style={{paddingTop: '10px'}}>
+                  <IconNotifications  />
+                </IconButton>
+              </Badge>
+
+              {/* stevie, veronika, jenny */}
+              <Avatar
+                src="https://semantic-ui.com/images/avatar/large/stevie.jpg"
+                size={30}
+                style={{margin: '15px 10px', cursor: 'pointer'}}
+              />
+              <IconMenu
+                  iconButtonElement={
+                      <IconButton style={{width: 50, height: 50, marginTop: 5, marginBottom: 5}}>
+                        <IconMoreVert />
+                      </IconButton>
+                  }
+                  anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+                  targetOrigin={{horizontal: 'right', vertical: 'top'}}
+                >
+                  <MenuItem primaryText="회원정보" />
+                  <MenuItem primaryText="수업목록" />
+              </IconMenu>
+              <IconButton style={{width: 50, height: 50, marginTop: 5, marginBottom: 5}}>
+                <IconHelpOutline />
+              </IconButton>
+
+            </div>
+          </div>
+
         </div>
       </header>
+
     );
   }
 };
