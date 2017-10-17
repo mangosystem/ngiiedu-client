2838e801d428c712cd57d3b355c668f557be3652
Youngman <klmo214558@gmail.com>
2017-10-17 16:29:54 +0900

    파일 업로드 css수정

--- ngiiedu-client/src/components/admin/school/sync/file/UploadDataContainer.js
+++ ngiiedu-client/src/components/admin/school/sync/file/UploadDataContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import Divider from 'material-ui/Divider';
+import './UploadDataContainer.css'
 
  const style={
         width:250,
@@ -64,10 +65,19 @@ class UploadDataContainer extends Component {
       <div>
          <h1 style={{padding:20}}>파일 선택</h1>
           <Divider style={{marginTop: '20px', marginBottom: '20px'}} />
-        <form onSubmit={this.handleSubmit.bind(this)} >
-          <input type="file" name="file" onChange={this.handleChange.bind(this)}/>
-          <button type="submit">Submit</button>
-        </form>
+          <div style={{marginLeft:150,marginTop:50}}>
+        <div className="filebox bs3-primary">
+                            <input className="upload-name" value="파일선택" disabled="disabled"/>
+
+                            <label for="ex_filename">업로드</label> 
+                          <input type="file" id="ex_filename" className="upload-hidden"/> 
+                </div>
+                </div>
+        {/* <form onSubmit={this.handleSubmit.bind(this)} > */}
+
+          {/* <input type="file" className="upload-name" name="file" onChange={this.handleChange.bind(this)}/> */}
+          {/* <button type="submit">Submit</button> */}
+        {/* </form> */}
       </div>
     );
   }
