c963f21f13e14f139e41cb027dbe3f22444808c4
thgmldkdlel <soheekang1123@gmail.com>
2017-10-24 11:15:41 +0900

    수업정보, 관련데이터 화면 개발

--- ngiiedu-client/src/components/courses/data/MainContainer.js
+++ ngiiedu-client/src/components/courses/data/MainContainer.js
@@ -5,11 +5,21 @@ import MenuPanel from '../common/MenuPanel.js';
 
 import Paper from 'material-ui/Paper';
 import Divider from 'material-ui/Divider';
+import IconDownload from 'material-ui/svg-icons/file/file-download';
 
 import IconMenu from 'material-ui/IconMenu';
 import IconButton from 'material-ui/IconButton';
 import MenuItem from 'material-ui/MenuItem';
 import IconMoreHoriz from 'material-ui/svg-icons/navigation/more-horiz';
+
+import FlatButton from 'material-ui/FlatButton';
+
+import {
+  Table,
+  TableBody,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
 
 class MainContainer extends React.Component {
 
@@ -35,6 +45,10 @@ class MainContainer extends React.Component {
       justifyContent: 'space-between'
     };
 
+    const style = {
+      textAlign: 'right'
+    };
+
     return (
       <main id="main">
 				<div className="inner">
@@ -49,7 +63,7 @@ class MainContainer extends React.Component {
               <Paper>
                 <div  style={{padding: '5px 20px 20px 20px'}}>
                   <div style={divStyle}>
-                  <div style={{fontSize: '20px', lineHeight: '30px'}}>수업 지도안</div>
+                  <div style={{fontSize: '20px', lineHeight: '20px'}}>수업 지도안</div>
                     <IconMenu
                       iconButtonElement={<IconButton><IconMoreHoriz /></IconButton>}
                       anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
@@ -58,17 +72,25 @@ class MainContainer extends React.Component {
                       <MenuItem primaryText="수정" />
                     </IconMenu>
                   </div>
-                  <Divider />
-                  <br />
-                  <ul style={{marginLeft: '10%'}}>
-                    <li>우리지역 소음지도 만들기</li>
-                  </ul>
+                  <Table selectable={false}>
+                    <TableBody displayRowCheckbox={false}>
+                        <TableRow>
+                            <TableRowColumn>우리지역 소음지도 만들기</TableRowColumn>
+                            <TableRowColumn style={style}>                              
+                              <FlatButton 
+                                label="download"
+                                labelPosition="before"
+                                icon={<IconDownload />} />                            
+                            </TableRowColumn>
+                        </TableRow>
+                    </TableBody>
+                  </Table>
                 </div>
               </Paper>
               <Paper>
                 <div  style={{padding: '5px 20px 20px 20px'}}>
                   <div style={divStyle}>
-                    <div style={{fontSize: '20px', lineHeight: '30px'}}>교사용 수업자료</div>
+                    <div style={{fontSize: '20px', lineHeight: '20px'}}>교사용 수업자료</div>
                     <IconMenu
                       iconButtonElement={<IconButton><IconMoreHoriz /></IconButton>}
                       anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
@@ -77,75 +99,163 @@ class MainContainer extends React.Component {
                       <MenuItem primaryText="수정" />
                     </IconMenu>
                   </div>
-                  <Divider />
-                  <br />
-                  <ul style={{marginLeft: '10%'}}>
-                    <li>1차시 - 교사용_소음 및 소음지도 개념</li>
-                    <li>2~3차시 - 교사용_구글맵 현장조사 매뉴얼</li>
-                    <li>2~3차시 - 콜렉터 현장조사 매뉴얼</li>
-                  </ul>
+                  <Table selectable={false}>
+                    <TableBody displayRowCheckbox={false}>
+                        <TableRow>
+                            <TableRowColumn>1차시 - 교사용_소음 및 소음지도 개념</TableRowColumn>
+                            <TableRowColumn style={style}>                              
+                              <FlatButton 
+                                label="download"
+                                labelPosition="before"
+                                icon={<IconDownload />} />                            
+                            </TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>2~3차시 - 교사용_구글맵 현장조사 매뉴얼</TableRowColumn>
+                            <TableRowColumn style={style}>                              
+                              <FlatButton 
+                                label="download"
+                                labelPosition="before"
+                                icon={<IconDownload />} />                            
+                            </TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>2~3차시 - 콜렉터 현장조사 매뉴얼</TableRowColumn>
+                            <TableRowColumn style={style}>                              
+                              <FlatButton 
+                                label="download"
+                                labelPosition="before"
+                                icon={<IconDownload />} />                            
+                            </TableRowColumn>
+                        </TableRow>
+                    </TableBody>
+                  </Table>
                 </div>
               </Paper>
               <Paper>
                 <div style={{padding: '5px 20px 20px 20px'}}>
                   <div style={divStyle}>
-                    <div style={{fontSize: '20px', lineHeight: '30px'}}>학생 활동지</div>
-                      <IconMenu
+                    <div style={{fontSize: '20px', lineHeight: '20px'}}>학생 활동지</div>
+                    <IconMenu
                         iconButtonElement={<IconButton><IconMoreHoriz /></IconButton>}
                         anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
                         targetOrigin={{horizontal: 'right', vertical: 'top'}}
-                      >
+                    >
                         <MenuItem primaryText="수정" />
-                      </IconMenu>
-                    </div>
-                  <Divider />
-                  <br />
-                  <ul style={{marginLeft: '10%'}}>
-                    <li>2~3차시 - 학생용_구글맵을 이용한 소음지도 만들기</li>
-                    <li>4~5차시 - 학생용_구글맵으로 소음지도 분석활동하기</li>
-                    <li>4~5차시 - 학생용_스토리맵 만들기 매뉴얼</li>
-                    <li>4~5차시 - 학생용_우리 지역 소음지도 해석하기</li>
-                  </ul>
-                </div>
-              </Paper>
-              <Paper>
-                <div style={{padding: '5px 20px 20px 20px'}}>
-                  <div style={divStyle}>
-                    <div style={{fontSize: '20px', lineHeight: '30px'}}>교사용 참고자료</div>
-                      <IconMenu
-                        iconButtonElement={<IconButton><IconMoreHoriz /></IconButton>}
-                        anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
-                        targetOrigin={{horizontal: 'right', vertical: 'top'}}
-                      >
-                        <MenuItem primaryText="수정" />
-                      </IconMenu>
-                    </div>
-                  <Divider />
-                  <br />
-                  <ul style={{marginLeft: '10%'}}>
-                    <li>2~3차시 - 참고자료_소음지도 지오데이터베이스 생성 매뉴얼</li>
-                  </ul>
-                </div>
-              </Paper>
-              <Paper>
-                <div style={{padding: '5px 20px 20px 20px'}}>
-                  <div style={divStyle}>
-                    <div style={{fontSize: '20px', lineHeight: '30px'}}>활동 매뉴얼</div>
-                      <IconMenu
-                        iconButtonElement={<IconButton><IconMoreHoriz /></IconButton>}
-                        anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
-                        targetOrigin={{horizontal: 'right', vertical: 'top'}}
-                      >
-                        <MenuItem primaryText="수정" />
-                      </IconMenu>
+                    </IconMenu>
                   </div>
-                  <Divider />
-                  <br />
-                  <ul style={{marginLeft: '10%'}}>
-                    <li>1차시 - 교사용_소음 및 소음지도 개념</li>
-                    <li>2~3차시 - 교사용_구글맵 현장조사 매뉴얼</li>
-                    <li>2~3차시 - 콜렉터 현장조사 매뉴얼</li>
-                  </ul>
+                  <Table selectable={false}>
+                    <TableBody displayRowCheckbox={false}>
+                        <TableRow>
+                            <TableRowColumn>2~3차시 - 학생용_구글맵을 이용한 소음지도 만들기</TableRowColumn>
+                            <TableRowColumn style={style}>                              
+                              <FlatButton 
+                                label="download"
+                                labelPosition="before"
+                                icon={<IconDownload />} />                            
+                            </TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>4~5차시 - 학생용_구글맵으로 소음지도 분석활동하기</TableRowColumn>
+                            <TableRowColumn style={style}>                              
+                              <FlatButton 
+                                label="download"
+                                labelPosition="before"
+                                icon={<IconDownload />} />                            
+                            </TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>4~5차시 - 학생용_스토리맵 만들기 매뉴얼</TableRowColumn>
+                            <TableRowColumn style={style}>                              
+                              <FlatButton 
+                                label="download"
+                                labelPosition="before"
+                                icon={<IconDownload />} />                            
+                            </TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>4~5차시 - 학생용_우리 지역 소음지도 해석하기</TableRowColumn>
+                            <TableRowColumn style={style}>                              
+                              <FlatButton 
+                                label="download"
+                                labelPosition="before"
+                                icon={<IconDownload />} />                            
+                            </TableRowColumn>
+                        </TableRow>
+                    </TableBody>
+                  </Table>
+                </div>
+              </Paper>
+              <Paper>
+                <div style={{padding: '5px 20px 20px 20px'}}>
+                  <div style={divStyle}>
+                    <div style={{fontSize: '20px', lineHeight: '20px'}}>교사용 참고자료</div>
+                    <IconMenu
+                      iconButtonElement={<IconButton><IconMoreHoriz /></IconButton>}
+                      anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+                      targetOrigin={{horizontal: 'right', vertical: 'top'}}
+                    >
+                      <MenuItem primaryText="수정" />
+                    </IconMenu>
+                  </div>
+                  <Table selectable={false}>
+                    <TableBody displayRowCheckbox={false}>
+                        <TableRow>
+                            <TableRowColumn colSpan="2">2~3차시 - 참고자료_소음지도 지오데이터베이스 생성 매뉴얼</TableRowColumn>
+                            <TableRowColumn style={style}>                              
+                              <FlatButton 
+                                label="download"
+                                labelPosition="before"
+                                icon={<IconDownload />} />                            
+                            </TableRowColumn>
+                        </TableRow>
+                    </TableBody>
+                  </Table>
+                </div>
+              </Paper>
+              <Paper>
+                <div style={{padding: '5px 20px 20px 20px'}}>
+                  <div style={divStyle}>
+                    <div style={{fontSize: '20px', lineHeight: '20px'}}>활동 매뉴얼</div>
+                    <IconMenu
+                        iconButtonElement={<IconButton><IconMoreHoriz /></IconButton>}
+                        anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+                        targetOrigin={{horizontal: 'right', vertical: 'top'}}
+                    >
+                        <MenuItem primaryText="수정" />
+                    </IconMenu>
+                  </div>
+                  <Table selectable={false}>
+                    <TableBody displayRowCheckbox={false}>
+                        <TableRow>
+                            <TableRowColumn>1차시 - 교사용_소음 및 소음지도 개념</TableRowColumn>
+                            <TableRowColumn style={style}>                              
+                              <FlatButton 
+                                label="download"
+                                labelPosition="before"
+                                icon={<IconDownload />} />                            
+                            </TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>2~3차시 - 교사용_구글맵 현장조사 매뉴얼</TableRowColumn>
+                            <TableRowColumn style={style}>                              
+                              <FlatButton 
+                                label="download"
+                                labelPosition="before"
+                                icon={<IconDownload />} />                            
+                            </TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>2~3차시 - 콜렉터 현장조사 매뉴얼</TableRowColumn>
+                            <TableRowColumn style={style}>                              
+                              <FlatButton 
+                                label="download"
+                                labelPosition="before"
+                                icon={<IconDownload />} />                            
+                            </TableRowColumn>
+                        </TableRow>
+                    </TableBody>
+                  </Table>
                 </div>
               </Paper>
             </section>
--- ngiiedu-client/src/components/courses/info/MainContainer.js
+++ ngiiedu-client/src/components/courses/info/MainContainer.js
@@ -4,6 +4,8 @@ import { withRouter } from "react-router-dom";
 import Paper from 'material-ui/Paper';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
 
 import Divider from 'material-ui/Divider';
 import Subheader from 'material-ui/Subheader';
@@ -11,8 +13,9 @@ import Subheader from 'material-ui/Subheader';
 import IconMenu from 'material-ui/IconMenu';
 import IconButton from 'material-ui/IconButton';
 import FontIcon from 'material-ui/FontIcon';
+import IconEdit from 'material-ui/svg-icons/image/edit';
 
-import {List, ListItem} from 'material-ui/List';
+import { List, ListItem } from 'material-ui/List';
 
 import IconMoreHoriz from 'material-ui/svg-icons/navigation/more-horiz';
 
@@ -20,87 +23,132 @@ import MenuPanel from '../common/MenuPanel.js';
 
 class MainContainer extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       isAccessor: true,
       isOwner: true,
-      isMember: false
-    }
+      isMember: false,
+      isEdit: false,
+      courseInfo: {}
+    };
   }
-  
+
   componentWillMount() {
     // alert('생성자, 참여자 구분하여 UI 구성');
-    console.log(this.props.match.params.COURSEID);
+
+  }
+
+  componentDidMount() {
+
+    const courseid = this.props.match.params.COURSEID;
+
+    $.ajax({
+        url: 'http://localhost:8080/ngiiedu/api/v1/courses/' + courseid + '.json',
+        dataType: 'json',
+        cache: false,
+        success: function(data) {
+            const courseInfo = JSON.parse(JSON.stringify(data)).response.data;
+
+            console.log(courseInfo);
+            this.setState({courseInfo: courseInfo});
+
+        }.bind(this),
+        error: function(xhr, status, err) {
+            console.error(status, err.toString());
+        }.bind(this)
+    });
   }
 
   render() {
     return (
       <main id="main">
-
         <div className="inner">
-          <navi>
-            <Paper style={{marginBottom: 20}}>
-              <div style={{paddingTop: 20, paddingBottom: 20, textAlign: 'center', position: 'relative'}}>
-                <h2 style={{margin: '10px auto'}}>
-                  수업이름 표시
-                </h2>
-                <div style={{margin: '10px auto 20px'}}>
-                  교사가 입력한 간단한 수업내용 표시
-                </div>
-                <div style={{position: 'absolute', right: 10, bottom: 5}}>
-                  <IconButton><FontIcon><i className="fa fa-cog"></i></FontIcon></IconButton>
-                </div>
-              </div>
-            </Paper>
-          </navi>
-
           <div className="flexible">
-
             <MenuPanel
               isAccessor={this.state.isAccessor}
               isOwner={this.state.isOwner}
               isMember={this.state.isMember}
             />
-
             <section>
               <Paper>
-                <div style={{paddingTop: 15, paddingBottom: 10}}>
-                  <div style={{fontSize: '2rem', fontWeight: 200, display: 'flex', padding: '0 20px'}}>
-                    <div style={{lineHeight: '48px', margin: '0 auto'}}>우리지역 소음지도 만들기</div>
-                    <div>
-                      <IconMenu
-                        iconButtonElement={<IconButton><IconMoreHoriz /></IconButton>}
-                        anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
-                        targetOrigin={{horizontal: 'right', vertical: 'top'}}
-                      >
-                        <MenuItem primaryText="수정" />
-                      </IconMenu>
-                    </div>
-                  </div>
-                </div>
-                <Divider />
-                <div style={{padding: 20, marginBottom: 20}}>
-                  <div style={{fontSize: '1.5rem', fontWeight: 200}}>
-                    우리지역 소음지도 만들기 소음은 우리의 일상생활에 일어나는 현상이지만 지나치게 큰 소음은 생활환경을 불편하게 하는 요소가 될 수 있다. 평소에 무심코 지나칠 수 있는 소음을 스마트폰 측정앱을 이용하여 측정해보고, 소음의 원인, 불편함을 느끼는 정도를 확인하고, 저감할 수 있는 대책은 무엇인지를 생각해보는 활동을 수행한다.
-                  </div>
-                </div>
+                {(() => {
+                  if (!this.state.isEdit)
+                    return (
+                      <div>
+                        <div style={{ paddingTop: 15, paddingBottom: 10 }}>
+                          <div style={{ 
+                            fontSize: '1.4rem', fontWeight: 200, display: 'flex', padding: '0 20px',
+                            alignItems: 'center', justifyContent: 'space-between' 
+                          }}>
+                            <div style={{ lineHeight: '2rem' }}>{this.state.courseInfo.courseName}</div>
+                            {(() => {
+                              if (this.state.isAccessor && this.state.isOwner) 
+                                return (
+                                  <div>
+                                    <IconButton>
+                                      <FontIcon onClick={() => this.setState({isEdit: true})}><IconEdit /></FontIcon>
+                                    </IconButton>
+                                  </div>
+                                );
+                            })()}
+                          </div>
+                        </div>
+                        <div style={{ padding: 20, marginBottom: 20 }}>
+                          <div style={{ fontSize: '1rem', fontWeight: 200 }}>
+                            {this.state.courseInfo.courseMetadata}
+                            우리지역 소음지도 만들기 소음은 우리의 일상생활에 일어나는 현상이지만 지나치게 큰 소음은 생활환경을 불편하게 하는 요소가 될 수 있다. 평소에 무심코 지나칠 수 있는 소음을 스마트폰 측정앱을 이용하여 측정해보고, 소음의 원인, 불편함을 느끼는 정도를 확인하고, 저감할 수 있는 대책은 무엇인지를 생각해보는 활동을 수행한다.
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  else 
+                    return (
+                      <div>
+                        <div style={{ paddingTop: 15, paddingBottom: 10 }}>
+                          <div style={{ display: 'flex', padding: '0 20px' }}>
+                            <TextField
+                              id="title"
+                              inputStyle={{fontSize: '1.4rem', fontWeight: 200}}
+                              defaultValue={this.state.courseInfo.courseName}
+                              fullWidth={true}
+                            />
+                          </div>
+                        </div>
+                        <div style={{ padding: 20, marginBottom: 20 }}>
+                          <TextField
+                            id="detail"
+                            defaultValue="우리지역 소음지도 만들기 소음은 우리의 일상생활에 일어나는 현상이지만 지나치게 큰 소음은 생활환경을 불편하게 하는 요소가 될 수 있다. 평소에 무심코 지나칠 수 있는 소음을 스마트폰 측정앱을 이용하여 측정해보고, 소음의 원인, 불편함을 느끼는 정도를 확인하고, 저감할 수 있는 대책은 무엇인지를 생각해보는 활동을 수행한다."
+                            multiLine={true}
+                            fullWidth={true}
+                          />                          
+                        </div>
+                        <div style={{textAlign: 'right'}}>
+                          <FlatButton label="취소" style={{margin: '5px'}}
+                            onClick={() => this.setState({isEdit: false})}/>
+                          <FlatButton label="저장" primary={true}  style={{margin: '5px'}}
+                            onClick={() => this.setState({isEdit: false})}
+                            icon={<i className="fa fa-check" aria-hidden="true"></i>}/>
+                        </div>
+                        <br />
+                      </div>
+                    );
+                })()}
               </Paper>
-
               {(() => {
                 if (this.state.isAccessor && this.state.isOwner)
-                  return(
+                  return (
                     <Paper>
-                      <div style={{paddingTop: 15, paddingBottom: 10}}>
-                        <div style={{fontSize: '1.5rem', display: 'flex', padding: '0 20px'}}>
-                          <div style={{lineHeight: '48px', margin: '0 auto'}}>
+                      <div style={{ paddingTop: 15, paddingBottom: 10 }}>
+                        <div style={{ fontSize: '1.5rem', display: 'flex', padding: '0 20px' }}>
+                          <div style={{ lineHeight: '48px', margin: '0 auto' }}>
                             수업코드 : CAP TOZ
                           </div>
                           <div>
                             <IconMenu
                               iconButtonElement={<IconButton><IconMoreHoriz /></IconButton>}
-                              anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
-                              targetOrigin={{horizontal: 'right', vertical: 'top'}}
+                              anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                              targetOrigin={{ horizontal: 'right', vertical: 'top' }}
                             >
                               <MenuItem primaryText="코드 변경" />
                             </IconMenu>
@@ -110,9 +158,7 @@ class MainContainer extends React.Component {
                     </Paper>
                   );
               })()}
-
             </section>
-
           </div>
         </div>
       </main>
