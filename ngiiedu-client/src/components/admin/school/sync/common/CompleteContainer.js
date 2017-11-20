a63cf0afe12faacb2a34647c63712ddca8e27468
Youngman <klmo214558@gmail.com>
2017-11-20 14:14:55 +0900

    학교목록 동기화 로딩화면

--- ngiiedu-client/src/components/admin/school/sync/common/CompleteContainer.js
+++ ngiiedu-client/src/components/admin/school/sync/common/CompleteContainer.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import Divider from 'material-ui/Divider';
 import './CompleteContainer.css';
+import CircularProgress from 'material-ui/CircularProgress';
 
 class CompleteContainer extends Component {
+
+    constructor(){
+        super();
+
+        this.state={
+            complete:false
+        }
+    }
+
+    componentWillReceiveProps(nextProps){
+        this.setState({
+            complete:true
+        })
+    }
+
     render() {
         return (
             <div>
                 <h1 style={{padding:20}}>동기화 완료</h1>
+
                 <Divider style={{marginTop: '20px', marginBottom: '20px'}} />
+                {this.state.complete ? 
                 <div className="CompleteContainer">
                         신규 데이터 :
                         {this.props.newRow}
@@ -16,6 +34,12 @@ class CompleteContainer extends Component {
                         중복 데이터 : 
                         {this.props.overlapRow}
                 </div>
+                :
+                <div className="CompleteContainer">
+                    <CircularProgress size={80} thickness={5} />
+                </div>
+                }
+
             </div>
         );
     }
